734dfc3411b363319772a2734253d1031c96cf31
ertugrulcetin <ertu.ctn@gmail.com>
2022-03-21 00:17:33 +0300

    update project clj

--- modules/core/project.clj
+++ modules/core/project.clj
@@ -1,7 +1,7 @@
-(defproject metosin/tilakone.core "0.0.5-SNAPSHOT"
+(defproject org.clojars.ertucetin/tilakone.core "0.0.5-SNAPSHOT"
   :description "Minimal finite state machine library"
-  :url "https://github.com/metosin/tilakone"
-  :scm {:name "git", :url "https://github.com/metosin/tilakone"}
+  :url "https://github.com/ertugrulcetin/tilakone"
+  :scm {:name "git", :url "https://github.com/ertugrulcetin/tilakone"}
   :dependencies []
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
